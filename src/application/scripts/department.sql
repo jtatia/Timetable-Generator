32bf0cb614bec83eb06624d41850448f0d7b66d1
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-11-25 01:56:50 +0530

    sql chnged

--- src/application/scripts/department.sql
+++ src/application/scripts/department.sql
@@ -4,7 +4,7 @@ use abc;
 
 drop table if exists department;
 
-CREATE TABLE department(id varchar(10) PRIMARY KEY,deptName varchar(30) NOT NULL);
+CREATE TABLE department(id varchar(10) PRIMARY KEY,deptName varchar(50) NOT NULL);
 
 INSERT INTO department value('D01','Computer Science and Engineering');
 INSERT INTO department value('D02','Mechanical Engineering');
