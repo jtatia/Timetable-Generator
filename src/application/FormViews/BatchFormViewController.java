c28899dbf27a1d0882dd81286ab4c586e9b3562c
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-11-25 03:07:36 +0530

    working on timtableview

--- src/application/FormViews/BatchFormViewController.java
+++ src/application/FormViews/BatchFormViewController.java
@@ -1,13 +1,17 @@
 package application.FormViews;
 
 import application.DatabaseUtils.BatchDAO;
+import application.TableViews.BatchTableViewController;
 import application.TimetableClasses.Batch;
 import application.TimetableClasses.Department;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class BatchFormViewController {
 
@@ -27,10 +31,11 @@ public class BatchFormViewController {
     private TextField DepartmentIdTextField;
 
     @FXML
-    void onSubmitAction(ActionEvent event) {
+    void onSubmitAction(ActionEvent event) throws IOException {
         String deptId=BatchIdTextField.getText();
         String deptName= DepartmentIdTextField.getText();
         Batch department = new Batch(deptId,deptName);
         BatchDAO.insertBatch(department);
+
     }
 }
