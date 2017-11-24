ee6b1d3799e5515d4fe6f300f4f6d3892c3f7c9f
EmberCS15 <abhijitroy996@gmail.com>
2017-11-24 23:44:16 +0530

    Changes made to algorithm

--- src/application/TimetableAlgorithm.java
+++ src/application/TimetableAlgorithm.java
@@ -8,11 +8,13 @@ import java.util.Random;
 
 public class TimetableAlgorithm {
     final int row = 5, column=6;
-    public boolean isCourseTeacherPresent(ArrayList<ArrayList<TimetableSlots>> timetable,Doublet d,Batch b){
+    public boolean checkTimetable(ArrayList<ArrayList<TimetableSlots>> timetable,Triplet t){
         for(int i=0;i<row;i++){
             for(int j=0;j<column;j++){
-                if(timetable.get(i).get(j).isDoubletPresent(d)){
-                    timetable.get(i).get(j).insertBatch(b);
+                int position = timetable.get(i).get(j).isTeacherCoursePresent(t.getTeacher(),t.getCourse());
+                if(position != -1){
+                    //timetable.get(i).get(j).insertBatch(t.getBatch());
+                    timetable.get(i).get(j).insertBatch(t.getBatch(),position);
                     return true;
                 }
             }
@@ -23,10 +25,10 @@ public class TimetableAlgorithm {
     public List<ArrayList<TimetableSlots>> createTimetable(ArrayList<Triplet> tripletList) {
         int i,j;
         ArrayList<ArrayList<TimetableSlots>> timetable = new ArrayList<ArrayList<TimetableSlots>>();
-        ArrayList<ArrayList<Doublet>> dayList = new ArrayList<ArrayList<Doublet>>(row);
+        ArrayList<ArrayList<Course>> dayList = new ArrayList<ArrayList<Course>>(row);
         for(i=0;i<row;i++){
             timetable.add(new ArrayList<TimetableSlots>());
-            dayList.add(new ArrayList<Doublet>());
+            dayList.add(new ArrayList<Course>());
         }
         for(i=0;i<row;i++){
             for(j=0;j<column;j++){
@@ -35,44 +37,46 @@ public class TimetableAlgorithm {
         }
         for(Triplet t:tripletList){
             int f = t.getCourse().getFrequencyOfCourse();
-            Doublet doublet = new Doublet(t.getTeacher(),t.getCourse());
+            Course cr = t.getCourse();
+            //Doublet doublet = new Doublet(t.getTeacher(),t.getCourse());
             while(f!=0){
                 boolean found = false;
-                if(isCourseTeacherPresent(timetable,doublet,t.getBatch())){
+                //Buggy Same batch may already be present on that day
+                if(checkTimetable(timetable,t)){
                     f--;
                     continue;
                 }
-                for(i=0;i<row;i++){
-                    if(dayList.get(i).contains(doublet)){
+                for(i=0;i<row;i++) {
+                    if (dayList.get(i).contains(cr)) {
                         continue;
                     }
                     Random rand = new Random();
                     //See random number generation maybe buggy
-                    int randomPeriod = rand.nextInt(((row-1)-0)+1);
-                    for(j=0;j<column;j++){
-                        TimetableSlots ts =timetable.get(i).get(randomPeriod);
-                        if(!ts.isTeacherPresent(doublet.getDTeacher()) && !ts.isBatchPresent(t.getBatch())){
-                            found = true;
-                            ts.insertDoublet(doublet);
+                    int randomPeriod = rand.nextInt(((column - 1) - 0) + 1);
+                    for (j = 0; j < column; j++) {
+                        TimetableSlots ts = timetable.get(i).get(randomPeriod);
+                        if (!ts.isTeacherPresent(t.getTeacher()) && !ts.isBatchPresent(t.getBatch())) {
+                            Doublet newEntry = new Doublet(t.getTeacher(), t.getCourse(), t.getBatch());
+                            ts.insertDoublet(newEntry);
                             ts.insertBatch(t.getBatch());
+                            ts.insertTeacher(t.getTeacher());
                             timetable.get(i).set(randomPeriod,ts);
-                            dayList.get(i).add(doublet);
-                            break;
+                            dayList.get(i).add(t.getCourse());
                         }
-                        randomPeriod = (randomPeriod+j)%column;
+                        randomPeriod = (randomPeriod + j) % column;
                     }
                 }
                 if(!found){
                     for(i=0;i<row;i++){
                         for(j=0;j<column;j++){
                             TimetableSlots ts =timetable.get(i).get(j);
-                            if(!ts.isTeacherPresent(doublet.getDTeacher()) && !ts.isBatchPresent(t.getBatch())){
-                                found = true;
-                                ts.insertDoublet(doublet);
+                            if(!ts.isTeacherPresent(t.getTeacher()) && !ts.isBatchPresent(t.getBatch())){
+                                Doublet newEntry = new Doublet(t.getTeacher(), t.getCourse(), t.getBatch());
+                                ts.insertDoublet(newEntry);
                                 ts.insertBatch(t.getBatch());
+                                ts.insertTeacher(t.getTeacher());
                                 timetable.get(i).set(j,ts);
-                                dayList.get(i).add(doublet);
-                                break;
+                                dayList.get(i).add(t.getCourse());
                             }
                         }
                     }
--- src/application/TimetableClasses/Doublet.java
+++ src/application/TimetableClasses/Doublet.java
@@ -1,12 +1,34 @@
 package application.TimetableClasses;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Teacher -> teaches -> course -> to ->batchList
 public class Doublet {
     private Teacher teacher;
     private Course course;
+    private ArrayList<Batch> listOfBatch;
 
     public Doublet(Teacher teacher, Course course) {
+        this.listOfBatch = new ArrayList<Batch>();
         this.teacher = teacher;
         this.course = course;
+    }
+
+    public Doublet(Teacher teacher, Course course,Batch b) {
+        this.listOfBatch = new ArrayList<Batch>();
+        this.listOfBatch.add(b);
+        this.teacher = teacher;
+        this.course = course;
+    }
+
+    public void insertDoubletBatch(Batch b){
+        this.listOfBatch.add(b);
+    }
+
+    public List<Batch> getDoubletBatchList(){
+        return this.listOfBatch;
     }
 
     public Teacher getDTeacher() {
